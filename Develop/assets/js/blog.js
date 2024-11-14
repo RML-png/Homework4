727a076e22011b5e752b51f66eb75218894c04ec
rosemarie.lupi <Rosemarie.lupi123@gmail.com>
2024-11-14 17:43:10 -0500

    Homework4

--- Develop/assets/js/blog.js
+++ Develop/assets/js/blog.js
@@ -6,33 +6,44 @@ const backButton = document.getElementById('back');
 
 // Function to create and append a new div element
 function createAndAppendElement(content, className) {
-    const newDiv = document.createElement('div');
-    newDiv.textContent = content;
-    newDiv.className = className;
-    mainElement.appendChild(newDiv);
+    const newDiv = document.createElement('article');
+    const h2El = document.createElement("h2")
+    const h3El = document.createElement ("h3")
+    const pEl = document.createElement("p")
+    h3El.textContent ="User: "+content.username
+    h2El.textContent = "Blog: "+content.title;
+    pEl.textContent = "Content: "+content.content;
+    newDiv.classList.add(className);
+    newDiv.appendChild(h2El)
+    newDiv.appendChild(h3El)
+    newDiv.appendChild(pEl)
+    return newDiv
 }
 
 // Call the function to create and append a new div
-createAndAppendElement('This is a new div element!', 'my-new-div');
+//createAndAppendElement('This is a new div element!', 'my-new-div');
 
 // Function to handle the case where there are no blog posts to display
 function noPostsFunction() {
-    createAndAppendElement("No Blog posts yet...", 'my-new-div');
+    const h4El = document.createElement("h4")
+    h4El.textContent = "No Blog posts yet...", 'my-new-div'
+    mainElement.appendChild(h4El)
 }
 
 // Function to render the list of blog posts
 function renderBlogList() {
-    const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
-
+    const blogPosts = readLocalStorage()
     if (blogPosts.length > 0) {
         blogPosts.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.innerHTML = `
-                <h3>${post.title}</h3>
-                <p>By: ${post.username}</p>
-                <p>${post.content}</p>
-            `;
-            document.getElementById('blogList').appendChild(postElement);
+            const postElement = createAndAppendElement(post,"card")
+            mainElement.appendChild(postElement);
+            //  document.createElement('div');
+            // postElement.innerHTML = `
+            //     <h3>${post.title}</h3>
+            //     <p>By: ${post.username}</p>
+            //     <p>${post.content}</p>
+            // `;
+            //document.getElementById('blogList').appendChild(postElement);
         });
     } else {
         noPostsFunction();
@@ -49,7 +60,5 @@ backButton.addEventListener('click', () => {
     redirectPage('index.html'); 
 });
 
-// Redirect function
-function redirectPage(url) {
-    location.assign(url);
-}
+
+   // renderBlogList()
